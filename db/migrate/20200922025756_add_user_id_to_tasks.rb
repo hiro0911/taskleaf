d7b8e288ff7b37dc2a88f6cdec841b8defa87de6
Hiromichi Matsui <evergreen.0911@gmail.com>
2020-09-22 09:08:49 +0000

    [Add]４章修了

---
+++ db/migrate/20200922025756_add_user_id_to_tasks.rb
@@ -0,0 +1,10 @@
+class AddUserIdToTasks < ActiveRecord::Migration[5.2]
+  def up
+  	execute 'DELETE FROM tasks;'
+  	add_reference :tasks, :user
+  	change_column :tasks, :user_id, :integer, null: false
+  end
+  def down
+  	remove_reference :tasks, :user, index: true
+  end
+end
