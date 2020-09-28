41e544f30b3a3d9ed9f86a121045ecbb624641c4
Hiromichi Matsui <evergreen.0911@gmail.com>
2020-09-28 01:39:51 +0000

    [Add]７章修了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,17 @@ class TasksController < ApplicationController
 	before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
   	@q = current_user.tasks.ransack(params[:q])
-  	@tasks = @q.result(distinct: true)
+  	@tasks = @q.result(distinct: true).page(params[:page])
+
+  	respond_to do |format|
+  		format.html
+  		format.csv { send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv"}
+  	end
+  end
+
+  def import
+  	current_user.tasks.import(params[:file])
+  	redirect_to tasks_url, notice: "タスクを追加しました"
   end
 
   def show
@@ -28,6 +38,7 @@ class TasksController < ApplicationController
 
   	if @task.save
   		TaskMailer.creation_email(@task).deliver_now
+  		SampleJob.perform_later
   		redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
   	else
   		render :new
@@ -50,7 +61,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-  	params.require(:task).permit(:name, :description)
+  	params.require(:task).permit(:name, :description, :image)
   end
 
   def set_task
