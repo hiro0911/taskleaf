0093920ff5b53fe8c4444c7055705b23e84c61aa
Hiromichi Matsui <evergreen.0911@gmail.com>
2020-09-30 08:39:58 +0000

    [Add]非同期通信

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -8,3 +8,13 @@ dicument.addEventListener('turbolinks:load', function(){
 		});
 	});
 });
+
+document.addEventListener('turbolinks:load', function(){
+	document.querySelectorAll('.delete').forEach(function(a){
+		a.addEventListener('ajax:success', function(){
+			var td = a.parentNode;
+			var tr = td.parentNode;
+			tr.style.display = 'none';
+		});
+	});
+});
